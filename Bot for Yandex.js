6343ef7cf1da93ee7196c7cb23fba4e56e2cf2b8
jozi2020 <80878958+jozi2020@users.noreply.github.com>
2021-03-23 13:26:02 +0300

    Update Bot for Yandex.js

--- Bot for Yandex.js
+++ Bot for Yandex.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        http://www.yandex.ru/*
+// @match        http://yandex.ru/*
 // @grant        none
 // ==/UserScript==
 
